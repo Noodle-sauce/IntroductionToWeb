9155ffb789c35977506c335cd4dff3380f5a2a7f
Ajdin Omeragic <ajdin.omeragic@stu.ibu.edu.ba>
2023-04-02 16:31:57 +0200

    merge.fix

--- exerc.php
+++ exerc.php
@@ -1,4 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   </head>
+   <body>
+   <form method='POST'>
+   <h2>Please input your name:</h2>
+ <input type="text" name="name">
+ <input type="submit" value="Submit Name">
+ </form>
 <?php
-echo "Tomorrow I \'ll learn PHP global variables."."\n"; 
-echo "This is a bad command : del c:\\*.*"."\n"; 
+//Retrieve name from query string and store to a local variable
+$name = $_POST['name'];
+echo "<h3> Hello $name </h3>";
 ?>
+</body>
+</html>
